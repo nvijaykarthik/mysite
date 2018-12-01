38636e83a9b4c8ebb845e71eba7a07fbbec1d054
nvijaykarthik <nvijaykarthik@gmail.com>
2018-12-01 22:31:00 +0530

    Adding home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import axios from 'axios';
+import Home  from './Component/Home';
+
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home} />
+    </Switch>
+  </main>
+)
+
+const Menu = () => (
+  <nav className="navbar navbar-expand-lg navbar-light navbar-dark bg-primary fixed-top">
+    <div className="container">
+      <a className="navbar-brand" href="index.html">
+        Vijaykarthik n
+    </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul className="navbar-nav ml-auto mr-sm-2 mt-2 mt-lg-0">
+          <li className="nav-item active mr-3">
+            <Link to='/' className='nav-link'>Home</Link>
+          </li>
+          <li className="nav-item mr-3">
+            <a className="nav-link" href="#">resume</a>
+          </li>
+          <li className="nav-item mr-3">
+            <a className="nav-link" href="#">blog</a>
+          </li>
+          <li className="nav-item mr-3">
+            <a className="nav-link" href="#">Contacts</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+)
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header>
+          <Menu />
         </header>
+        <main>
+          <Main />
+        </main>
       </div>
     );
   }
---
+++ src/Component/Home.jsx
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <section className="u-content-space">
+                    <div className="container">
+                        <div className="row ">
+                            <div className="col-md-3">
+                                <img alt="" width="300" className="u-box-shadow-lg img-fluid rounded-circle mt-1" src="https://avatars1.githubusercontent.com/u/7747313?s=460&amp;v=4"></img>
+                            </div>
+                            <div className="col-md-9">
+                                <h1>VijayKarthik Nagarajan</h1>
+                                <h4><i>Full Stack/Polyglot: Architect</i></h4>
+                            </div>`
+                    </div>
+                    </div>
+                </section>
+                <section class="bg-secondary py-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center text-md-left mb-3 mb-md-0 text-white">
+                                <span class=" ">?</span>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="bg-light u-content-space">
+                    <div class="container">
+                        <header class="text-center w-md-50 mx-auto mb-8">
+                            <h2 class="h1">Skills</h2>
+                            <p class="h5">Stream creative technology company providing key digital services for everyone.</p>
+                        </header>
+                        <div class="row">
+                            <div class="col-lg-4 mb-5 mb-lg-0">
+                                <div class="display-4 text-primary mb-2">
+                                    <i class="fab fa-grav"></i>
+                                </div>
+                                <h4 class="h5">Excellent Features</h4>
+                                <p>Achieve virtually any look and layout design within one UI Kit solution.</p>
+                            </div>
+
+                            <div class="col-lg-4 mb-5 mb-lg-0">
+                                <div class="display-4 text-primary mb-2">
+                                    <i class="fab fa-connectdevelop"></i>
+                                </div>
+                                <h4 class="h5">Powerful API</h4>
+                                <p>We strive to figure out ways to help your audience grow through all platforms.</p>
+                            </div>
+
+                            <div class="col-lg-4 mb-5 mb-lg-0">
+                                <div class="display-4 text-primary mb-2">
+                                    <i class="fas fa-cubes"></i>
+                                </div>
+                                <h4 class="h5">Blockchain Based</h4>
+                                <p>Find what you need in one template and combine features at will.</p>
+                            </div>
+                        </div>
+                        <hr class="my-8"></hr>
+
+                        <div class="row">
+                            <div class="col-lg-4 mb-5 mb-lg-0">
+                                <div class="display-4 text-primary mb-2">
+                                    <i class="fab fa-vuejs"></i>
+                                </div>
+                                <h4 class="h5">VueJS Included</h4>
+                                <p>Easy and fast adjustments of elements are possible with Stream UI Kit.</p>
+                            </div>
+
+                            <div class="col-lg-4 mb-5 mb-lg-0">
+                                <div class="display-4 text-primary mb-2">
+                                    <i class="fab fa-react"></i>
+                                </div>
+                                <h4 class="h5">React Powered</h4>
+                                <p>Our React Powered App will save you tons of time and cost in your projects.</p>
+                            </div>
+
+                            <div class="col-lg-4 mb-5 mb-lg-0">
+                                <div class="display-4 text-primary mb-2">
+                                    <i class="fab fa-angular"></i>
+                                </div>
+                                <h4 class="h5">Angular Compatible</h4>
+                                <p>Power your app with Angular components for no cost within one stop solution.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+                <section class="u-content-space">
+                    <div class="container">
+                        <header class="text-center w-md-50 mx-auto mb-8">
+                            <h2 class="h1">Opensource Contribution</h2>
+                            <p class="h5">Your business needs a little push. We strive to figure out ways to help your audience grow.</p>
+                        </header>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-sm-6 mb-5">
+                                <div class="card u-box-shadow-lg ">
+                                    <img class="card-img-top" src="https://jsblight.com/cdwd/wp-content/uploads/2015/01/About-1-400x550.jpg" alt="Card image cap"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6 mb-5">
+                                <div class="card u-box-shadow-lg ">
+                                    <img class="card-img-top" src="https://jsblight.com/cdwd/wp-content/uploads/2015/01/About-1-400x550.jpg" alt="Card image cap"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6 mb-5">
+                                <div class="card u-box-shadow-lg">
+                                    <img class="card-img-top" src="https://jsblight.com/cdwd/wp-content/uploads/2015/01/About-1-400x550.jpg" alt="Card image cap"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-sm-6 mb-5">
+                                <div class="card u-box-shadow-lg">
+                                    <img class="card-img-top" src="https://jsblight.com/cdwd/wp-content/uploads/2015/01/About-1-400x550.jpg" alt="Card image cap"></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section class="bg-light u-content-space">
+                    <div class="container">
+                        <div class="row ">
+                            <div class="col-md-12">
+                                <header class="text-center w-md-50 mx-auto mb-8">
+                                    <h2 class="h1">Recent Blogs</h2>
+                                </header>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Home;
